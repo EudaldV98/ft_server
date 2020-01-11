3a998a43023292c7580f893305b83d4c488e12b7
Jan eudald VAQUER <jvaquer@e2r4p1.42.fr>
2020-01-11 12:24:26 +0100

    added own key and crt to test

--- srcs/wp-config.php
+++ srcs/wp-config.php
@@ -20,7 +20,7 @@
 /** Nom de la base de données de WordPress. */
 define('DB_NAME', 'wordpress');
 /** Utilisateur de la base de données MySQL. */
-define('DB_USER', 'wordpressuser');
+define('DB_USER', 'user');
 /** Mot de passe de la base de données MySQL. */
 define('DB_PASSWORD', '1234');
 /** Adresse de l’hébergement MySQL. */
